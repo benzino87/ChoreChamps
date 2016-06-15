aeaf07c64d7fbf3460df6defc66e939d4b7cdf07
Jason <benselj@mail.gvsu.edu>
2016-06-15 18:31:31 +0000

    changed alert to innrHTML during invalid inputs

--- welcome/champSetup.php
+++ welcome/champSetup.php
@@ -8,24 +8,30 @@ $numberOfKids = $_SESSION['kidNum'];
 <html>
     <link rel="stylesheet" type="text/css" href="champ.css" />
     <body>
+    <div class="champ">
         <div class="champSetup">
         <h2>We're almost done!</h2>
             <h4>Enter the name(s) of your kids<br>
             This will set up accounts for them<br>
             Once they have accounts they will login under your Group name<br>
             EX: TheStarks.Rob</h4>
-            
+        <div class="champForm"> 
         <form method="POST" action="createChamps.php" name="kidForm" onsubmit="return checkFields()">
             <?php
             for($i = 0; $i < $numberOfKids; $i++){
-                echo $i.". Kid name:<br>";
+                echo "Kid name:<br>";
                 echo "<input type=text name='$i'/><br>";
             }
             ?>
             <input type="submit" value="Submit"/>
         </form>
-    </body>
+        </div>
+        <div class="invalid_response">
+            <p id="response"></p>
+        </div>
+     </div>
     </div>
+    </body>
     <script type="text/javascript">
         var numOfKids = "<?php echo $numberOfKids?>";
         console.log(numOfKids);
@@ -33,7 +39,8 @@ $numberOfKids = $_SESSION['kidNum'];
             for(var i = 0; i < numOfKids; i++){
                 var field = document.forms["kidForm"][i].value;
                 if(field == "" || field == null){
-                    alert("You're missing a kid!");
+                    var response = document.getElementById("response");
+                    response.innerHTML = "You must enter names for each child";
                     return false;
                 }
             }
